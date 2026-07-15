90ca9e9d4f241d21338ba8541e5408480a718f1e
rkzzzz008 <glrahulkrishnan@gmail.com>
2026-07-15 10:09:31 +0530

    add:difference function

--- pyth.py
+++ pyth.py
@@ -2,3 +2,9 @@ a=int(input("Enter a number:"))
 b=int(input("Enter a number:"))
 c=a+b
 print("The sum of", a, "and", b, "is", c)
+d=int(input("Enter another number:"))
+if d>c:
+    result=d-c
+else:
+    result=c-d
+print("The difference between", d, "and", c, "is", result)    
